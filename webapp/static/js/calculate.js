fdd143aeb8872c1b900b6f0b93a161d0e7de2848
warex03 <warex03@gmail.com>
2013-08-30 10:18:24 +0800

    Added a progress bar in the results section.

--- webapp/static/js/calculate.js
+++ webapp/static/js/calculate.js
@@ -24,11 +24,20 @@ $(function() {
     });
     
     $("#calculate")[0].onclick = function(){
+        //Fix the <br> tags with correct css please
+        var msg = 'Please wait while the system is calculating the results...'
+        var br = '<br><br><br><br><br><br><br><br>';
+        var progressbar = '<div class="progress progress-striped active">' +
+            '<div class="progress-bar"  role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div>';
+        $("#accordion").accordion("option", "active", 1);
+        $("#results").html(br +msg+ progressbar)
         calculate(exposure, hazard);
     };
     
     $("#reset")[0].onclick = function(){
-        
+        var br = '<br><br><br><br><br><br><br><br>';
+        var msg = 'Please input the necessary data(i.e. exposure, hazard layers).';
+        $("#results").html(br + msg);
     };
 });
 
@@ -42,9 +51,7 @@ function mapInit() {
 function calculate(exposure, hazard){
     $.post("/calculate")
     .done(function(data){
-        $("#results_header").html('Results' + '');
         $("#results").html(data);
-        $("#accordion").accordion("option", "active", 1);
     })
     .fail(function(data){
         alert("POST request to '/calculate' failed!");
